4b28c28b581c77d2726b8d5ce8d9a37818e4dba8
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-04-14 20:13:11 +0530

    api routes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,7 @@ const PORT = 4000;
 const express = require("express");
 const app = express();
 const connectDB = require("./DB/db");
-const apiRoutes = require("./app");
+ const apiRoutes = require("./app");
 
 const dotenv = require("dotenv")
 dotenv.config({
@@ -15,7 +15,7 @@ dotenv.config({
 // Connect to MongoDB
 connectDB()
   .then(() => {
-    app.use(apiRoutes);
+   // app.use(apiRoutes);
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
     });
