bc873a75b48cd878058b746824b4ee5426ae3147
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-04-17 00:23:53 +0530

    css for list product

--- Admin/src/components/ListProduct/ListProduct.jsx
+++ Admin/src/components/ListProduct/ListProduct.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./ListProduct.css"
+import { useState } from 'react'
+import cross_icon from "../../assets/cross_icon.png"
 
 const ListProduct = () => {
+
+  const [allProducts, setAllProducts] = useState([])
+
+  // const fetchInfo = async()=>{
+  //   await fetch('http://localhost:4000/allproduct')
+  //   .then((res)=>res.json())
+  //   .then((data) => {setAllProducts(data)})
+  // }
+
+  const fetchInfo = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/allproduct');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      // const data = await response.json();
+      // setAllProducts(data);
+      const responseData = await response.json();
+      setAllProducts(responseData.data); // Set allProducts to the data array
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Optionally, you can set allProducts to a default value or handle the error in another way.
+    }
+  };
+
+
+  useEffect(() => {
+    fetchInfo()
+  }, [])
+
+
   return (
-    <div className='list-product' >ListProduct</div>
+    <div className='list-product' >
+      <h1>All Product List </h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+        </div>
+        <div className="listproduct-allproducts">
+          {/* <hr /> */}
+         
+          {allProducts.map((product, index) => {
+            return <div key={index} className="listproduct-fromat-main  listproduct-format">
+              <img src={product.image} alt="" className="listproduct-product-icon" />
+              <p> {product.name} </p>
+              <p> ${product.old_price} </p>
+              <p> ${product.new_price} </p>
+              <p>{product.category} </p>
+              <img className='listproduct-remove-icon' src={cross_icon} alt="" />
+            </div>
+          })}
+        </div>
+     
+    </div>
   )
 }
 
--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -6,9 +6,17 @@ import { Link, NavLink } from 'react-router-dom'
 import nav_dropdown from '../Assets/dropdown_icon.png'
 import { ShopContext } from '../../Context/ShopContext'
 export const Nav = () => {
+
+
+ 
+
+    
+
+// prev
     const [menu , setmenu] = useState("shop");
     const menuRef = useRef()
 
+     
     const dropToggle = (e)=>{
         menuRef.current.classList.toggle('nav-menu-visible');
          e.target.classList.toggle('open')
@@ -28,9 +36,14 @@ export const Nav = () => {
                 <li onClick={()=>{setmenu("shop")}}> <Link to = '/' style={{textDecoration:"none" ,  color: "black"}}>Shop</Link> {menu === "shop" ?<hr />   : <></>} </li>
                 <li onClick={()=>{setmenu("men")}}> <Link to='/men' style={{textDecoration:"none" , color: "black"}}>Men </Link> {menu === "men" ?<hr /> : <></>}</li>
                 <li onClick={()=>{setmenu("women")}}> <Link to='/women' style={{textDecoration:"none" , color: "black"}} >Women</Link>  {menu === "women" ?<hr /> : <></>}</li>
-
                 <li onClick={()=>{setmenu("kid")}}> <Link to = '/kid' style={{textDecoration:"none" , color: "black"}}>Kids</Link>  {menu === "kid" ?<hr /> : <></>}</li>
             </ul>
+
+       
+
+
+       
+
             <div className="nav-login-cart">
                 <Link to='/login'> <button>Login</button> </Link>
                <Link to='/cart'><img src={cart_icon} alt="" /></Link> 
